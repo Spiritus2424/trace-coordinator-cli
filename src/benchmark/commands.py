d8a8954525ac62a74fbcc047b95a91e1f02d5fa1
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-11-10 03:16:59 +0000

    wip

---
+++ src/benchmark/commands.py
@@ -0,0 +1,24 @@
+from click import command, argument, option, Path, prompt
+
+
+@command(name="open-trace")
+def open_trace():
+    pass
+
+
+@command(name="get-trace")
+def get_trace():
+    pass
+
+@command(name="delete-trace")
+def delete_trace():
+    pass
+
+
+@command(name="open-experiment")
+def open_experiment():
+    pass
+
+@command(name="get-experiment")
+def get_experiment():
+    pass
