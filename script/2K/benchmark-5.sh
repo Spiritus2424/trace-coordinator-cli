187c22afb7d64f82932b0a7ecb4b963208a23582
Ahmad Faour <ahmad.faour@polymtl.ca>
2024-02-29 22:35:38 +0000

    WIP

--- script/2K/benchmark-5.sh
+++ script/2K/benchmark-5.sh
@@ -49,8 +49,8 @@ trace-coordinator benchmark -d --ip $IP -p $PORT \
     concrete-get-timegraph-states -v --nb-times 2560 --process "soma_base.gnu_m" --nb-items 50 \
         "cde21a0a-35cd-37d4-8fdc-d1ecad068f6b" \
         "org.eclipse.tracecompass.internal.analysis.os.linux.core.threadstatus.ThreadStatusDataProvider"\
-        1702056042965447210\
-        1702058405022656104
+        1702056042987417600\
+        1702056602949809200
 
 
 # Get TimeGraph Arrows: Thread Status Provider (org.eclipse.tracecompass.internal.analysis.os.linux.core.threadstatus.ThreadStatusDataProvider)
@@ -58,8 +58,8 @@ trace-coordinator benchmark -d --ip $IP -p $PORT \
     concrete-get-timegraph-arrows -v --nb-times 2560 \
         "cde21a0a-35cd-37d4-8fdc-d1ecad068f6b" \
         "org.eclipse.tracecompass.internal.analysis.os.linux.core.threadstatus.ThreadStatusDataProvider" \
-        1702056042965447210\
-        1702058405022656104
+        1702056042987417600\
+        1702056602949809200
 
 # Get XY Tree - CPU Usage (org.eclipse.tracecompass.analysis.os.linux.core.cpuusage.CpuUsageDataProvider)
 trace-coordinator benchmark -d --ip $IP -p $PORT \
@@ -72,5 +72,5 @@ trace-coordinator benchmark -d --ip $IP -p $PORT \
     concrete-get-xy -v --nb-times 2560 --process "soma_base.gnu_m" --nb-items 50 \
         "cde21a0a-35cd-37d4-8fdc-d1ecad068f6b" \
         "org.eclipse.tracecompass.analysis.os.linux.core.cpuusage.CpuUsageDataProvider" \
-        1702056042965447210\
-        1702058405022656104
+        1702056042987417600\
+        1702056602949809200
